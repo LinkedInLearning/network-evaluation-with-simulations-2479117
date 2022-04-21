57f70c35b0700a3bce4ec3be75c22f77bf293410
ryan <30502544+RyanEduChannel@users.noreply.github.com>
2022-04-21 16:08:08 -0400

    Initial commit

---
+++ ns3-mmwave-4.0/README.md
@@ -0,0 +1,96 @@
+# mmWave ns-3 module #
+
+This is an [ns-3](https://www.nsnam.org "ns-3 Website") module for the simulation
+of 5G cellular networks operating at mmWaves. A description of this module can be found in [this paper](https://ieeexplore.ieee.org/document/8344116/ "mmwave paper").
+
+Main features:
+
+* Support of a wide range of channel models, including the model based on 3GPP TR 38.901 for frequencies between 0.5 and 100 GHz. Ray tracing and measured traces can also be used.
+
+* Custom PHY and MAC classes supporting the 3GPP NR frame structure and numerologies.
+
+* Custom schedulers for supporting dynamic TDD formats
+
+* Carrier Aggregation at the MAC layer
+
+* Enhancements to the RLC layer with re-segmentation of packets for retransmissions
+
+* Dual Connectivity with LTE base stations, with fast secondary cell handover and channel tracking
+
+* Simulation of core network elements (with also the MME as a real node)
+
+## Installation
+This repository contains a complete ns-3 installation with the addition of the mmwave module. 
+
+Use these commands to download and build `ns3-mmwave`:
+```
+git clone https://github.com/nyuwireless-unipd/ns3-mmwave.git
+cd ns3-mmwave
+./waf configure --disable-python --enable-examples && ./waf build
+```
+
+## Usage example
+You can use the following command to run the `mmwave-simple-epc` example. 
+```
+./waf --run mmwave-simple-epc
+```
+Other examples are included in `src/mmwave/examples/`
+
+## Documentation
+The documentation of this module is available at [this link](./src/mmwave/doc/mmwave-doc.md).
+
+## Related modules
+- MilliCar is an ns-3 module for the simulation of mmWave NR V2X networks. Check [this repo](https://github.com/signetlabdei/millicar) for further details.
+- A seperate module is being developed for [mmWave UE Energy Consumption](https://github.com/arghasen10/mmwave-energy "mmwave-energy"). You can use this module for analyzing 
+Energy Consumption behaviour of mmwave UE. Check this repository for further details.
+- `ns3-mmwave-iab` is an extended version of `ns3-mmWave` adding wireless relaying capabilities to an ns-3 NetDevice, and the possibility of simulating in-band relaying at mmWave frequencies. Check [this repo](https://github.com/signetlabdei/ns3-mmwave-iab) for further details.
+
+## References 
+The following papers describe in detail the features implemented in the mmWave
+module:
+- [End-to-End Simulation of 5G mmWave Networks](https://ieeexplore.ieee.org/document/8344116/ "comst paper") is a comprehensive tutorial with a detailed description of the whole module. We advise the researchers interested in this module to start reading from this paper;
+- [Integration of Carrier Aggregation and Dual Connectivity for the ns-3 mmWave Module](https://arxiv.org/abs/1802.06706 "wns3 2018") describes the Carrier Aggregation implementation;
+- [Implementation of A Spatial Channel Model for ns-3](https://arxiv.org/abs/2002.09341 "wns3 2020") describes the integration of the spatial channel model based on the 3GPP specifications TR 38.901 V15.0.0;
+- [Performance Comparison of Dual Connectivity and Hard Handover for LTE-5G Tight Integration](https://arxiv.org/abs/1607.05425 "simutools paper") describes the Dual Connectivity feature.
+
+These other papers describe features that were implemented in older releases: 
+- [ns-3 Implementation of the 3GPP MIMO Channel Model for Frequency Spectrum above 6 GHz](https://dl.acm.org/citation.cfm?id=3067678 "wns3 2017") describes the implementation of the 3GPP channel model based on TR 38.900;
+- [Multi-Sector and Multi-Panel Performance in 5G mmWave Cellular Networks](https://arxiv.org/abs/1808.04905 "globecom2018") describes the multi-sector addition to the 3GPP channel model;
+
+If you use this module in your research, please cite:
+
+M. Mezzavilla, M. Zhang, M. Polese, R. Ford, S. Dutta, S. Rangan, M. Zorzi, _"End-to-End Simulation of 5G mmWave Networks,"_ in IEEE Communications Surveys & Tutorials, vol. 20, no. 3, pp. 2237-2263, thirdquarter 2018. [bibtex available here](https://ieeexplore.ieee.org/document/8344116/)
+
+## Future work
+We are actively developing new features for the mmWave module, including:
+- 3GPP NR beam tracking
+- 3GPP NR Integrated Access and Backhaul feature (see [this repo](https://github.com/signetlabdei/ns3-mmwave-iab) for more details)
+
+## About
+This module is being developed by [NYU Wireless](http://wireless.engineering.nyu.edu/) and the [University of Padova](http://mmwave.dei.unipd.it/).
+This  work  was  supported  in  part by  the  U.S.  Department  of  Commerce  National  Institute  of  Standards  and Technology through the Project “An End-to-End Research Platform for Public Safety  Communications  above  6  GHz”  under  Award  70NANB17H16.
+
+
+
+<!-- The new-handover branch offers integration between LTE and mmWave and dual connectivity features.
+ -->
+
+## Authors ##
+
+The ns-3 mmWave module is the result of the development effort carried out by different people. The main contributors are: 
+- Tommaso Zugno, University of Padova
+- Michele Polese, University of Padova
+- Matteo Pagin, University of Padova
+- Mattia Lecci, University of Padova
+- Matteo Drago, University of Padova
+- Mattia Rebato, University of Padova
+- Menglei Zhang, NYU Wireless
+- Marco Giordani, University of Padova
+- Marco Mezzavilla, NYU Wireless
+- Sourjya Dutta, NYU Wireless
+- Russell Ford, NYU Wireless
+- Gabriel Arrobo, Intel
+
+## License ##
+
+This software is licensed under the terms of the GNU GPLv2, as like as ns-3. See the LICENSE file for more details.
---
+++ ns3-mmwave-4.0/src/mmwave/model/mmwave-amc.cc
@@ -0,0 +1,365 @@
+/* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
+/*
+ *   Adapted from the release 1.0 of the 5G-LENA simulator, please refer to https://5g-lena.cttc.es/ for further details 
+ *   and https://gitlab.com/cttc-lena/nr/-/tree/v1.0 for the reference code.
+ * 
+ *   Copyright (c) 2019 Centre Tecnologic de Telecomunicacions de Catalunya (CTTC)
+ *   Copyright (c) 2021 University of Padova, Dep. of Information Engineering, SIGNET lab.
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License version 2 as
+ *   published by the Free Software Foundation;
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include "mmwave-amc.h"
+#include <ns3/log.h>
+#include <ns3/double.h>
+#include <ns3/math.h>
+#include <ns3/enum.h>
+#include <ns3/uinteger.h>
+#include <ns3/object-factory.h>
+#include <ns3/mmwave-lte-mi-error-model.h>
+#include "mmwave-spectrum-value-helper.h"
+
+namespace ns3 {
+
+namespace mmwave {
+
+NS_LOG_COMPONENT_DEFINE ("MmWaveAmc");
+NS_OBJECT_ENSURE_REGISTERED (MmWaveAmc);
+
+MmWaveAmc::MmWaveAmc ()
+{
+  NS_ABORT_MSG ("This constructor should not be used!");
+}
+
+MmWaveAmc::MmWaveAmc (const Ptr<MmWavePhyMacCommon> phyMacParams)
+  : m_phyMacConfig {phyMacParams}
+{
+
+}
+
+MmWaveAmc::~MmWaveAmc ()
+{
+}
+
+void
+MmWaveAmc::SetDlMode ()
+{
+  NS_LOG_FUNCTION (this);
+  m_emMode = MmWaveErrorModel::DL;
+}
+
+void
+MmWaveAmc::SetUlMode ()
+{
+  NS_LOG_FUNCTION (this);
+  m_emMode = MmWaveErrorModel::UL;
+}
+
+TypeId
+MmWaveAmc::GetTypeId (void)
+{
+  static TypeId tid = TypeId ("ns3::MmWaveAmc")
+    .SetParent<Object> ()
+    .AddAttribute ("AmcModel",
+                   "AMC model used to assign CQI",
+                   EnumValue (MmWaveAmc::ErrorModel),
+                   MakeEnumAccessor (&MmWaveAmc::SetAmcModel,
+                                     &MmWaveAmc::GetAmcModel),
+                   MakeEnumChecker (MmWaveAmc::ErrorModel, "ErrorModel",
+                                    MmWaveAmc::ShannonModel, "ShannonModel"))
+    .AddAttribute ("Ber",
+                   "The target BER, used for assigning the MCS"
+                   "This parameter is used only by the ShannonModel",
+                   DoubleValue (0.00001),
+                   MakeDoubleAccessor (&MmWaveAmc::SetBer),
+                   MakeDoubleChecker <double> ())
+    .AddAttribute ("ErrorModelType",
+                   "Type of the Error Model to use when AmcModel is set to ErrorModel. "
+                   "This parameter has to match the ErrorModelType in mmwave-spectrum-model,"
+                   "because they need to refer to same MCS tables and indexes",
+                   TypeIdValue (MmWaveLteMiErrorModel::GetTypeId ()),
+                   MakeTypeIdAccessor (&MmWaveAmc::SetErrorModelType,
+                                       &MmWaveAmc::GetErrorModelType),
+                   MakeTypeIdChecker ())
+    .AddConstructor <MmWaveAmc> ()
+  ;
+  return tid;
+}
+
+TypeId
+MmWaveAmc::GetInstanceTypeId() const
+{
+  return MmWaveAmc::GetTypeId ();
+}
+
+void
+MmWaveAmc::SetBer (double ber)
+{
+  m_ber = ber;
+}
+
+uint8_t
+MmWaveAmc::GetMcsFromCqi (uint8_t cqi) const
+{
+  NS_LOG_FUNCTION (cqi);
+  NS_ASSERT_MSG (cqi >= 0 && cqi <= 15, "CQI must be in [0..15] = " << cqi);
+
+  double spectralEfficiency = m_errorModel->GetSpectralEfficiencyForCqi (cqi);
+  uint8_t mcs = 0;
+
+  while ((mcs < m_errorModel->GetMaxMcs ()) && (m_errorModel->GetSpectralEfficiencyForMcs (mcs + 1) <= spectralEfficiency))
+    {
+      ++mcs;
+    }
+
+  NS_LOG_LOGIC ("mcs = " << mcs);
+
+  return mcs;
+}
+
+uint32_t
+MmWaveAmc::CalculateTbSize (uint8_t mcs, uint8_t nSym) const
+{
+  NS_LOG_FUNCTION (this << +mcs);
+
+  NS_ASSERT_MSG (mcs <= m_errorModel->GetMaxMcs (), "MCS=" << +mcs <<
+                 " while maximum MCS is " << +(m_errorModel->GetMaxMcs ()));
+
+  uint32_t payloadSize = GetPayloadSize (mcs, nSym);
+  uint32_t tbSize = payloadSize;
+
+  if (payloadSize >= m_crcLen)
+    {
+      tbSize = payloadSize - m_crcLen;  //subtract parity bits of m_crcLen used in transport block
+    }
+  uint32_t cbSize = m_errorModel->GetMaxCbSize (payloadSize, mcs); // max size of a code block (including m_crcLen)
+  if (tbSize > cbSize)  // segmentation of the transport block occurs
+    {
+      double C = ceil (tbSize / cbSize);
+      tbSize = payloadSize - static_cast<uint32_t> (C * m_crcLen);   //subtract bits of m_crcLen used in code blocks, in case of code block segmentation
+    }
+
+  NS_LOG_INFO (" mcs:" << (unsigned) mcs << " TB size:" << tbSize);
+
+  return tbSize;
+}
+
+uint8_t 
+MmWaveAmc::GetMinNumSymForTbSize (uint32_t tbSize, uint8_t mcs) const
+{
+  uint32_t effTbSize {0};
+  uint8_t numSym {0};
+  while (effTbSize < tbSize && numSym < m_phyMacConfig->GetSymbPerSlot ())
+  {
+    numSym++;
+    effTbSize = CalculateTbSize (mcs, numSym);
+  }
+  NS_ABORT_MSG_IF (effTbSize < tbSize, "No way to create such TB size, something went wrong!");
+
+  return numSym;
+}
+
+uint32_t
+MmWaveAmc::GetPayloadSize (uint8_t mcs, uint8_t nSym) const
+{
+  uint32_t effNumRb {static_cast<uint32_t>(nSym)*m_phyMacConfig->GetNumRb ()};
+  return m_errorModel->GetPayloadSize (MmWavePhyMacCommon::SUBCARRIERS_PER_RB - MmWavePhyMacCommon::REF_SUBCARRIERS_PER_RB,
+                                       mcs, effNumRb, m_emMode);
+}
+
+uint8_t
+MmWaveAmc::CreateCqiFeedbackWbTdma (const SpectrumValue& sinr, uint8_t &mcs) const
+{
+  NS_LOG_FUNCTION (this);
+
+  // produces a single CQI/MCS value
+
+  //std::vector<int> cqi;
+  uint8_t cqi = 0;
+  double seAvg = 0;
+  double mcsAvg = 0;
+  double cqiAvg = 0;
+
+  Values::const_iterator it;
+  if (m_amcModel == ShannonModel)
+    {
+      uint32_t rbNum = 0;
+      for (it = sinr.ConstValuesBegin (); it != sinr.ConstValuesEnd (); it++)
+        {
+          double itSinr = (*it);
+          if (itSinr == 0.0)
+            {
+              //cqi.push_back (-1); // SINR == 0 (linear units) means no signal in this RB
+            }
+          else
+            {
+              /*
+               * Compute the spectral efficiency from the SINR
+               *                                        SINR
+               * spectralEfficiency = log2 (1 + -------------------- )
+               *                                    -ln(5*BER)/1.5
+               * NB: SINR must be expressed in linear units
+               */
+
+              double se = log2 ( 1 + ( itSinr / ( (-std::log (5.0 * m_ber )) / 1.5) ));
+              seAvg += se;
+
+              int cqi = GetCqiFromSpectralEfficiency (se);
+              mcsAvg += GetMcsFromSpectralEfficiency (se);
+              cqiAvg += cqi;
+              rbNum++;
+
+              NS_LOG_LOGIC (" PRB =" << sinr.GetSpectrumModel ()->GetNumBands ()
+                                     << ", sinr = " << itSinr
+                                     << " (=" << 10 * std::log10 (itSinr) << " dB)"
+                                     << ", spectral efficiency =" << se
+                                     << ", CQI = " << cqi << ", BER = " << m_ber);
+              //cqi.push_back (cqi);
+            }
+        }
+      seAvg /= rbNum;
+      mcsAvg /= rbNum;
+      cqiAvg /= rbNum;
+      cqi = ceil (cqiAvg);  //GetCqiFromSpectralEfficiency (seAvg);
+      mcs = GetMcsFromSpectralEfficiency (seAvg);   //ceil(mcsAvg);
+    }
+  else if (m_amcModel == ErrorModel)
+    {
+      std::vector <int> rbMap;
+      int rbId = 0;
+      for (it = sinr.ConstValuesBegin (); it != sinr.ConstValuesEnd (); it++)
+        {
+          if (*it != 0.0)
+            {
+              rbMap.push_back (rbId);
+            }
+          rbId += 1;
+        }
+
+      mcs = 0;
+      Ptr<MmWaveErrorModelOutput> output;
+      while (mcs <= m_errorModel->GetMaxMcs ())
+        {
+          output = m_errorModel->GetTbDecodificationStats (sinr, 
+                                                           rbMap,
+                                                           CalculateTbSize (mcs, 1), // TODO: check that the number of RBs is right
+                                                           mcs,
+                                                           MmWaveErrorModel::MmWaveErrorModelHistory ());
+          if (output->m_tbler > 0.1)
+            {
+              break;
+            }
+          mcs++;
+        }
+
+      if (mcs > 0)
+        {
+          mcs--;
+        }
+
+      if ((output->m_tbler > 0.1) && (mcs == 0))
+        {
+          cqi = 0;
+        }
+      else if (mcs == m_errorModel->GetMaxMcs ())
+        {
+          cqi = 15;   // all MCSs can guarantee the 10 % of BER
+        }
+      else
+        {
+          double s = m_errorModel->GetSpectralEfficiencyForMcs (mcs);
+          cqi = 0;
+          while ((cqi < 15) && (m_errorModel->GetSpectralEfficiencyForCqi (cqi + 1) <= s))
+            {
+              ++cqi;
+            }
+        }
+      NS_LOG_DEBUG (this << "\t MCS " << (uint16_t)mcs << "-> CQI " << cqi);
+    }
+  return cqi;
+}
+
+uint8_t
+MmWaveAmc::GetCqiFromSpectralEfficiency (double s) const
+{
+  NS_LOG_FUNCTION (s);
+  NS_ASSERT_MSG (s >= 0.0, "negative spectral efficiency = " << s);
+  uint8_t cqi = 0;
+  while ((cqi < 15) && (m_errorModel->GetSpectralEfficiencyForCqi (cqi + 1) < s))
+    {
+      ++cqi;
+    }
+  NS_LOG_LOGIC ("cqi = " << cqi);
+  return cqi;
+}
+
+uint8_t
+MmWaveAmc::GetMcsFromSpectralEfficiency (double s) const
+{
+  NS_LOG_FUNCTION (s);
+  NS_ASSERT_MSG (s >= 0.0, "negative spectral efficiency = " << s);
+  uint8_t mcs = 0;
+  while ((mcs < m_errorModel->GetMaxMcs ()) && (m_errorModel->GetSpectralEfficiencyForMcs (mcs + 1) < s))
+    {
+      ++mcs;
+    }
+  NS_LOG_LOGIC ("cqi = " << mcs);
+  return mcs;
+}
+
+uint32_t
+MmWaveAmc::GetMaxMcs() const
+{
+  NS_LOG_FUNCTION (this);
+  return m_errorModel->GetMaxMcs ();
+}
+
+void
+MmWaveAmc::SetAmcModel (MmWaveAmc::AmcModel m)
+{
+  NS_LOG_FUNCTION (this);
+  m_amcModel = m;
+}
+
+MmWaveAmc::AmcModel
+MmWaveAmc::GetAmcModel () const
+{
+  NS_LOG_FUNCTION (this);
+  return m_amcModel;
+}
+
+void
+MmWaveAmc::SetErrorModelType (const TypeId &type)
+{
+  NS_LOG_FUNCTION (this);
+  ObjectFactory factory;
+  m_errorModelType = type;
+
+  factory.SetTypeId (m_errorModelType);
+  m_errorModel = DynamicCast<MmWaveErrorModel> (factory.Create ());
+  NS_ASSERT (m_errorModel != nullptr);
+}
+
+TypeId
+MmWaveAmc::GetErrorModelType () const
+{
+  NS_LOG_FUNCTION (this);
+  return m_errorModelType;
+}
+
+
+} // end namespace mmwave
+
+} // end namespace ns3
